89bb17c403560d06c376c668aae4d42a7ca49130
Jordan Rivera <jordan.rivera34@gmail.com>
2019-03-15 17:05:33 -0400

    follower

--- tools/console.rb
+++ tools/console.rb
@@ -11,12 +11,14 @@ cult1 = Cult.new("Digi Cult", "Digital Underground", 1999, "All humans must die"
 cult2 = Cult.new("Mole Women", "Ohio", 2015, "I know Karate")
 cult3 = Cult.new("Dunder Mifflin", "Scranton", 2005, "That's what she said")
 
-follower1 = Follower.new("Michael", 43, "That's what she said", cult3)
-follower2 = Follower.new("Dwight", 35, "MICHAEL!", cult3)
-follower3 = Follower.new("Kimmy", 40, "Defeat the red demon", cult2)
-follower4 = Follower.new("Tidus", 43, "I'm on broadway", cult2)
-follower5 = Follower.new("Fred", 16, "I love fortnite", cult1)
-follower6 = Follower.new("Cyborg", 18, "Hacker life", cult1)
+follower1 = Follower.new("Michael", 43, "That's what she said")
+follower2 = Follower.new("Dwight", 35, "MICHAEL!")
+follower3 = Follower.new("Kimmy", 40, "Defeat the red demon")
+follower4 = Follower.new("Tidus", 43, "I'm on broadway")
+follower5 = Follower.new("Fred", 16, "I love fortnite")
+follower6 = Follower.new("Cyborg", 18, "Hacker life")
+
+
 
 
 
